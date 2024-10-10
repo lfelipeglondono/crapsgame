14c81a6b15d07d3fdb599e083fd17718ed391246
Alex Imbacuan <alex.imbacuan@correounivalle.edu.co>
2024-10-09 23:53:00 -0500

    Descripción de los cambios en Game.java

--- src/main/java/org/example/crapsgame/model/Game.java
+++ src/main/java/org/example/crapsgame/model/Game.java
@@ -15,26 +15,47 @@ public class Game {
     }
 
     public int getShoot() {
+
         return this.shoot;
     }
 
     public int getShootCount() {
+        if (shoot!=2 && shoot!=3 && shoot!=12){
+            shootCount++;
+        }
         return this.shootCount;
     }
 
     public int getPoint() {
+        if (shoot==5||shoot==6||shoot==8||shoot==9||shoot==5){
+            point=shoot;
+        }
+
         return this.point;
     }
 
     public boolean isWin() {
+        win=false;
+        if (shoot==7|| shoot==11){
+            lose=true;
+        }
+        //si captura el punto antes de sacar 7
+        if(shoot==point)
+            win =true;
         return this.win;
     }
 
     public boolean isLose() {
+        lose=false;
+        if (shoot==2|| shoot==3||shoot==12){
+            lose=true;
+        }
         return this.lose;
     }
 
-    public int rollDices() {
+    public int rollDices(int dado1, int dado2) {
+        shoot=dado1+dado2;
+
         return this.shoot;
     }
 
